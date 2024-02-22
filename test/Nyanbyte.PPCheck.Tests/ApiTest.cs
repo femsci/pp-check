a52665dd08c12209ba4ef1c820f82e7f66b8b78c
femsci <nya@femboy.science>
2024-02-22 15:15:44 +0100

    initial test project

---
+++ test/Nyanbyte.PPCheck.Tests/ApiTest.cs
@@ -0,0 +1,17 @@
+using Nyanbyte.PPCheck.Lib;
+
+namespace Nyanbyte.PPCheck.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Meow()
+    {
+        ApiClient cli = new();
+    }
+}
